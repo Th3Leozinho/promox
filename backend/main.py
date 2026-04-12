afd56c0cc7b2199e5c165baae74e75a4b584dffb
th3oleano <leo_oleano@hotmail.com>
2026-04-12 14:01:49 -0300

    painel

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import declarative_base
 Base = declarative_base()
+from pydantic import BaseModel
 
 class Credencial(Base):
     __tablename__ = "credenciais"
